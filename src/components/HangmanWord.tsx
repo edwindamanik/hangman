44c908a0daa9d940259dd0cc457eba926b095263
Edwin Immanuel Damanik <edwindamanik5@gmail.com>
2024-11-11 17:09:19 +0700

    feat: new hangman web game

---
+++ src/components/HangmanWord.tsx
@@ -0,0 +1,41 @@
+type HangmandWordProps = {
+  guessedLetters: string[];
+  wordToGuess: string;
+  reveal?: boolean;
+};
+
+export default function HangmanWord({
+  guessedLetters,
+  wordToGuess,
+  reveal = false,
+}: HangmandWordProps) {
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: ".25em",
+        fontSize: "6rem",
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        fontFamily: "monospace",
+      }}
+    >
+      {wordToGuess.split("").map((letter, index) => (
+        <span style={{ borderBottom: ".1em solid black" }} key={index}>
+          <span
+            style={{
+              visibility:
+                guessedLetters.includes(letter) || reveal
+                  ? "visible"
+                  : "hidden",
+              color:
+                !guessedLetters.includes(letter) && reveal ? "red" : "black",
+            }}
+          >
+            {letter}
+          </span>
+        </span>
+      ))}
+    </div>
+  );
+}
